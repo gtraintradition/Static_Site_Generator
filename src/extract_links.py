f733a62906ea0995831a845522e8179efa13840d
gtraintradition <gtraintradition@gmail.com>
2024-12-30 16:18:39 +0100

    added extract_links_py, with some tests

---
+++ src/extract_links.py
@@ -0,0 +1,31 @@
+import re
+
+
+def extract_markdown_images(text):
+    # takes raw markdown text and returns a list of tuples, each tuple should contain the alt text and the URL of any markdown images
+
+    if not isinstance(text, str):
+        raise ValueError("Argument passed must be a string")
+
+
+    #pattern = r"\!\[(.*?)\]\((.*?://.*?\..*?)\)"     
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    filtered = re.findall(pattern, text)
+
+    return filtered
+
+
+def extract_markdown_links(text):
+    # takes raw markdown text and returns a list of tuples, each tuple should contain the anchor text and the URL
+
+    if not isinstance(text, str):
+        raise ValueError("Argument passed must be a string")
+
+    #pattern = r"\[(.*?)\]\((.*?://.*?\..*?)\)"
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    filtered = re.findall(pattern, text)
+
+    return filtered
+
+
+    
---
+++ src/test_extract_links.py
@@ -0,0 +1,88 @@
+import unittest
+
+from extract_links import extract_markdown_images, extract_markdown_links
+
+
+
+class Test_extract_markdown_images(unittest.TestCase):
+
+    def test_2_img_links_in_1_str(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [('rick roll', 'https://i.imgur.com/aKaOqIh.gif'), ('obi wan', 'https://i.imgur.com/fJRm4Vk.jpeg')])
+
+
+    def test_str_is_entire_img_link(self):
+        text = "![rick roll](https://i.imgur.com/aKaOqIh.gif)"
+        self.assertEqual(extract_markdown_images(text), [('rick roll', 'https://i.imgur.com/aKaOqIh.gif')])
+
+
+    def test_no_img_link_in_str(self):
+        text = "This is a test."
+        self.assertEqual(extract_markdown_images(text), [])
+
+
+    def test_empty_string(self):
+        text = ""
+        self.assertEqual(extract_markdown_images(text), [])
+
+
+    def test_arg_passed_not_str(self):
+        text = []
+        with self.assertRaises(ValueError, msg="Expected ValueError"):
+            extract_markdown_images(text)
+
+
+    ###### Boot.dev test
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+
+class Test_extract_markdown_links(unittest.TestCase):
+
+    def test_2_links_in_1_str(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(text), [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')])
+
+
+    def test_2_links_1_str(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(text), [('to boot dev', 'https://www.boot.dev'), ('to youtube', 'https://www.youtube.com/@bootdotdev')])
+
+
+    def test_no_link_in_str(self):
+        text = "This is a test."
+        self.assertEqual(extract_markdown_links(text), [])
+
+
+    def test_empty_string(self):
+        text = ""
+        self.assertEqual(extract_markdown_links(text), [])
+
+
+    def test_arg_passed_not_str(self):
+        text = []
+        with self.assertRaises(ValueError, msg="Expected ValueError"):
+            extract_markdown_links(text)
+
+    ###### Boot.dev test
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
